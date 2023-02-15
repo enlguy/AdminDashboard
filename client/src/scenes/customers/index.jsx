9882d7a35ddf8c09a2cdf0103f46c5d149a6714d
Jack <jack282323@gmail.com>
2023-02-15 11:30:50 +0100

    Updating from local

--- client/src/scenes/customers/index.jsx
+++ client/src/scenes/customers/index.jsx
@@ -70,7 +70,18 @@ const Customers = () => {
                 },
                 "& .MuiDataGrid-virtualScroller": {
                     backgroundColor: theme.palette.primary.light,
-                }
+                },
+                "& .MuiDataGrid-footerContainer": {
+                    backgroundColor: theme.palette.primary.light,
+                },
+                "& .MuiDataGrid-footerContainer": {
+                    backgroundColor: theme.palette.background.alt,
+                    color: theme.palette.secondary[100],
+                    WebkitBorderTopRightRadius: "none"
+                },
+                "& .MuiDataGrid-toolbarContainer .MuiButton-text": {
+                    color: `${theme.palette.secondary[200]} !important`,
+                },
             }}
             >
                 <DataGrid 
